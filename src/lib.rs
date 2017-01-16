c9f284193d216b54ff5b9e0c625d069ede50e8c1
Alexander Polyakov <apolyakov@beget.com>
2017-01-16 15:12:17 +0300

    More docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! Netlink is a linux kernel interface used for communication between
 //! kernel and userspace.
+//!
+//! `socket` module can be used to establish Netlink socket
+//! `packet` contains high level functions and traits
 #[macro_use]
 extern crate bitflags; 
 extern crate pnet;
