31f75422fa3fcd1716595cbf7a2755dd1643f3f2
Elijah Charles <elij@lypy.com>
2017-11-20 12:35:04 +0000

    Revert "Fixes bug with wrong endianess"
    This reverts commit 1dcc5708b613070c8155627aba56262b0ea59a3d.

--- build.rs
+++ build.rs
@@ -1,22 +1,25 @@
-extern crate pnet_macros;
 extern crate syntex;
+extern crate pnet_macros;
 
 use std::env;
 use std::path::Path;
 use std::fs;
 
-const FILES: &'static [&'static str] = &["netlink.rs", "route/route.rs", "audit/audit.rs"];
+const FILES: &'static [&'static str] = &[
+    "netlink.rs",
+    "route/route.rs",
+    "audit/audit.rs"
+];
 
-fn main() {
+pub fn expand() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
 
     for file in FILES {
         let src_file = format!("src/packet/{}.in", file);
         let src = Path::new(&src_file);
-
         let dst_name = Path::new(file);
         if let Some(parent) = dst_name.parent() {
-            let _ = fs::create_dir(Path::new(&out_dir).join(parent));
+            fs::create_dir(Path::new(&out_dir).join(parent));
         }
         let dst = Path::new(&out_dir).join(dst_name);
 
@@ -25,4 +28,8 @@ fn main() {
 
         registry.expand("", &src, &dst).unwrap();
     }
+}
+
+fn main() {
+    expand();
 }
